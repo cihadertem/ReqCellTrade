96442ac9e4195dc20b789a7cc449dbc5e53adc12
Cihad Ertem <cihadertem61@icloud.com>
2024-02-03 22:42:00 +0300

    0.0.5
    Requests kütüphanesi yerine Selenium kütüphanesine geçiş yapıldı.
    Site üzerindeki excel dosyasını indirmeyi başarı ile tamamladı.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 # Kullanıcıdan bilgileri alma
 kullanici_adi = input("Email: ")
 sifre = input("Şifre: ")
+kagit_adi = input("Finansal tablosu için kağıt adı girin: ")
 
 # Giriş bilgileri
 login_data = {
@@ -12,22 +17,47 @@ login_data = {
 
 # Giriş yapılacak sayfanın URL'si
 url = 'https://malitablolar.com/Aut/Login'
+finansal_tablo_url = f'https://malitablolar.com/finansal-tablo/{kagit_adi}/bilanco?kur=TL'
+
+# Tarayıcı sürücüsünü başlatma
+driver = webdriver.Chrome()
 
 # Oturum başlatma ve giriş yapma
-with requests.Session() as session:
-    response = session.get(url)
+driver.get(url)
 
-    # Giriş verilerini ekleyerek POST isteği gönderme
-    response = session.post(url, data=login_data)
+# Email alanını bulma ve giriş yapma
+email_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, 'Email'))
+)
+email_input.send_keys(kullanici_adi)
 
-    # Giriş sonrası HTTP durum koduna göre kontrol
-    if response.status_code == 200:
-        # Giriş sonrası sayfanın içeriğini kontrol et
-        if 'https://malitablolar.com/Analiz' in response.url:
-            print('Parola Doğru.')
-        elif 'https://malitablolar.com/Aut/Login' in response.url:
-            print('Şifre yanlış, tekrar deneyin.')
-        else:
-            print('Bilinmeyen bir durum oluştu.')
-    else:
-        print('Bilinmeyen bir durum oluştu.')
+# Şifre alanını bulma ve giriş yapma
+sifre_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, 'Sifre'))
+)
+sifre_input.send_keys(sifre)
+
+# Giriş butonuna tıklama
+sifre_input.submit()
+
+# Giriş sonrası HTTP durum koduna göre kontrol
+if 'https://malitablolar.com/Analiz' in driver.current_url:
+    print('Parola Doğru.')
+
+    # Finansal tablo sayfasına yönlendirme
+    driver.get(finansal_tablo_url)
+
+    # Excel butonunu bulma ve tıklama
+    try:
+        excel_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "buttons-excel"))
+        )
+        excel_button.click()
+        print("Excel butonuna tıklandı.")
+    except Exception as e:
+        print(f"Hata: Excel butonu bulunamadı veya tıklanamadı. {e}")
+else:
+    print('Şifre yanlış, tekrar deneyin.')
+
+# Tarayıcıyı kapatma
+driver.quit()
